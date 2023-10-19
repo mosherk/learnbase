6bc5e51f28e82da2d60a8780ab7d741f509ebce8
mosherk <2816507760@qq.com>
2023-10-19 23:30:04 +0800

    添加和修改一些

--- es6-尚硅谷/day4/22async和await读取文件.js
+++ es6-尚硅谷/day4/22async和await读取文件.js
@@ -1,50 +1,50 @@
 const fs = require('fs')
 
 //读取为学
-function readWeiXue(){
-    return new Promise((resolve,reject)=>{
-        fs.readFile('./resources/为学.md',(err,data)=>{
-            //如果失败
-            if(err) reject(err)
-            //如果成功
-            resolve(data)
-        })
-    })
+function readWeiXue() {
+	return new Promise((resolve, reject) => {
+		fs.readFile('./resources/为学.md', (err, data) => {
+			//如果失败
+			if (err) reject(err)
+			//如果成功
+			resolve(data)
+		})
+	})
 }
 
 //读取插秧诗
-function readChaYangShi(){
-    return new Promise((resolve,reject)=>{
-        fs.readFile('./resources/插秧诗.md',(err,data)=>{
-            //如果失败
-            if(err) reject(err)
-            //如果成功
-            resolve(data)
-        })
-    })
+function readChaYangShi() {
+	return new Promise((resolve, reject) => {
+		fs.readFile('./resources/插秧诗.md', (err, data) => {
+			//如果失败
+			if (err) reject(err)
+			//如果成功
+			resolve(data)
+		})
+	})
 }
 
 //读取观书有感
-function readGuanShu(){
-    return new Promise((resolve,reject)=>{
-        fs.readFile('./resources/观书有感.md',(err,data)=>{
-            //如果失败
-            if(err) reject(err)
-            //如果成功
-            resolve(data)
-        })
-    })
+function readGuanShu() {
+	return new Promise((resolve, reject) => {
+		fs.readFile('./resources/观书有感.md', (err, data) => {
+			//如果失败
+			if (err) reject(err)
+			//如果成功
+			resolve(data)
+		})
+	})
 }
 
 //声明async函数
-async function main(){
-    //获取为学
-    let weixue = await readWeiXue()
-    let chayang = await readChaYangShi()
-    let guanshu = await readGuanShu()
-    console.log(weixue.toString());
-    console.log(chayang.toString());
-    console.log(guanshu.toString());
+async function main() {
+	//获取为学
+	let weixue = await readWeiXue()
+	let chayang = await readChaYangShi()
+	let guanshu = await readGuanShu()
+	console.log(weixue.toString());
+	console.log(chayang.toString());
+	console.log(guanshu.toString());
 }
 // 弹幕：体现异步：只要有一个await语句后的promise对象是reject，
 // 那么整个async函数都会中断执行。
